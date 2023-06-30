8b1ba7f5d0d3248bc5f1186f2d53030819d9a65b
rupesh237 <rupesh237@icloud.com>
2023-06-30 16:30:12 +0545

    data channel added for msg

--- chatting_system/static/js/main.js
+++ chatting_system/static/js/main.js
@@ -1,6 +1,8 @@
 console.log('This is main.js !')
 
 
+var mapPeers= {};
+
 var usernameInput= document.querySelector('#username');
 var btnJoin = document.querySelector('#btn-join');
 
@@ -10,7 +12,20 @@ var webSocket;
 
 function webSocketOnMessage(event){
     var parseData= JSON.parse(event.data);
-    var message = parseData['messgae'];
+    var peerUsername = parseData['peer'];
+    var action = parseData['action'];
+
+    if (username == peerUsername){
+        return;
+    }
+
+    var receiver_channel_name = parseData['message']['receiver_channel_name'];
+
+    if (action == 'new-peer'){
+        createOfferer(peerUsername, receiver_channel_name);
+        return;
+    }
+
     console.log('message: ', message);
 }
 
@@ -47,10 +62,7 @@ btnJoin.addEventListener('click', ()=>{
     webSocket.addEventListener('open', (e)=>{
         console.log('Connection Opened');
 
-        var jsonStr= JSON.stringify({
-            "message": "This is a message",
-        });
-        webSocket.send(jsonStr);
+        sendSignal('new-peer', {})
     });
     webSocket.addEventListener('message', webSocketOnMessage);
     webSocket.addEventListener('close', (e)=>{
@@ -79,4 +91,94 @@ var userMedia = navigator.mediaDevices.getUserMedia(constraints)
     })
     .catch(error=>{
         console.log('Error accessing media devices ', error);
+    });
+
+
+
+
+function sendSignal(action, message){
+    var jsonStr= JSON.stringify({
+        'peer': username,
+        'action': action,
+        "message":message,
+    });
+    webSocket.send(jsonStr);
+}
+
+function createOfferer(peerUsername, receiver_channel_name){
+    var peer = new RTCPeerConnection(null);
+    addLocalTracks(peer);
+
+    var dc= peer.createDataChannel('channel');
+    dc.addEventListener('open', ()=>{
+        console.log('connection opened !');
+    });
+
+    dc.addEventListener('message', dcOnMessage);
+
+    var remoteVideo= createVideo(peerUsername);
+    setOnTrack(peer, remoteVideo);
+
+    mapPeers[peerUsername] = [peer, dc];
+
+    peer.addEventListener('iceconnectionstatechange', () =>{
+        var iceConnectionState = peer.iceConnectionState;
+
+        if(iceConnectionState === 'failed' || iceConnectionState === 'disconnected' || iceConnectionState === 'closed'){
+            delete mapPeers[peerUsername];
+            if (iceConnectionState != 'closed'){
+                peer.close();
+            }
+            removeVideo(remoteVideo);
+        }
     })
+
+}
+
+function addLocalTracks(peer){
+    localStream.getTracks().forEach(track=>{
+        peer.addTrack(track, localStream);
+    });
+    return;
+}
+
+var messageList= document.querySelector('#message-list');
+function dcOnMessage(event){
+    var message= event.data;
+
+    var li= document.createElement('li');
+    li.appendChild(document.createTextNode(message));
+    messageList.appendChild(li);
+}
+
+function createVideo(peerUsername){
+    var videoContainer= document.querySelector('#video-container');
+    var remoteVideo = document.createElement('video');
+
+    remoteVideo.id= peerUsername + '-video';
+    remoteVideo.autoplay = true;
+    remoteVideo.playsInline = true;
+
+    var videoWrapper = document.createElement('div');
+    videoContainer.appendChild(videoWrapper);
+    
+    videoWrapper.appendChild(remoteVideo);
+
+    return remoteVideo;
+}
+
+function setOnTrack(peer, remoteVideo){
+    var remoteStream = new MediaStream();
+
+    remoteVideo.srcObject = remoteStream;
+
+    peer.addEventListener('track', async (event)=>{
+        remoteStream.addTrack(event.track, remoteStream);
+    });
+}
+
+function removeVideo(Video){
+    var videoWrapper= video.parentNode;
+
+    videoWrapper.parentNode.removeChild(videoWrapper);
+}
